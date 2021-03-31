1d4ec21ee1bae6989582e7940c0763e31c2fdc82
KETANBHALERAO <bhalerao.ketan@gmail.com>
2021-04-01 00:47:17 +0530

    Test - Case - Dynamic value calculations

--- src/components/ColorCanvas.test.js
+++ src/components/ColorCanvas.test.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import useColorGenerator from "../hooks/colorGenerator";
-import PROJECT_CONSTANTS from "../config";
+import config from "../config";
 
 /**
  * This will call custom hook which generates color array
@@ -9,9 +9,9 @@ import PROJECT_CONSTANTS from "../config";
 
 test("Color Canvas - Check for unique colors", async () => {
   const [color_start, color_end, color_step] = [
-    PROJECT_CONSTANTS.COLOR_START,
-    PROJECT_CONSTANTS.COLOR_END,
-    PROJECT_CONSTANTS.STEPS
+    config.COLOR_START,
+    config.COLOR_END,
+    config.STEPS
   ];
 
   const color_list = [];
@@ -23,7 +23,35 @@ test("Color Canvas - Check for unique colors", async () => {
     color_list.push(comb);
   }
   const unique_color_list_size = new Set(color_list).size;
-  expect(unique_color_list_size === color_list.length).toBeTruthy();
 
-  expect(unique_color_list_size).toEqual(32768);
+  expect(unique_color_list_size === color_list.length).toBeTruthy();
+});
+
+/**
+ * This will call custom hook which generates color array
+ * Checks for uniqueness & size of array generated
+ */
+
+test("Color Canvas - count of colors", async () => {
+  const [color_start, color_end, color_step] = [
+    config.COLOR_START,
+    config.COLOR_END,
+    config.STEPS
+  ];
+  const total = Math.pow(
+    (color_end - color_start + color_step) / color_step,
+    3
+  );
+
+  const color_list = [];
+
+  for (let comb of useColorGenerator({
+    color_start: color_start,
+    color_end: color_end,
+    color_step: color_step
+  })) {
+    color_list.push(comb);
+  }
+
+  expect(new Set(color_list).size).toEqual(total);
 });
