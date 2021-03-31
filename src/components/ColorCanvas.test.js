6e0c85cc9604515ae1b6445c506fc7219415a0c1
KETANBHALERAO <bhalerao.ketan@gmail.com>
2021-03-31 23:59:48 +0530

    Removed unused Imports

--- src/components/ColorCanvas.test.js
+++ src/components/ColorCanvas.test.js
@@ -1,6 +1,4 @@
-import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import ColorCanvas from "./ColorCanvas";
 import useColorGenerator from "../hooks/colorGenerator";
 import PROJECT_CONSTANTS from "../config";
 
@@ -28,29 +26,4 @@ test("Color Canvas - Check for unique colors", async () => {
   expect(unique_color_list_size === color_list.length).toBeTruthy();
 
   expect(unique_color_list_size).toEqual(32768);
-});
-
-/**
- * This will render ColorCanvas Component
- * Checks for no of divs & style attribute
- */
-
-test("renders color canvas", async () => {
-  const [color_start, color_end, color_step] = [8, 16, 8];
-
-  render(
-    <ColorCanvas
-      color_start={color_start}
-      color_end={color_end}
-      color_step={color_step}
-    />
-  );
-
-  const items = await screen.findAllByRole("div");
-  expect(items).toHaveLength(8);
-
-  expect(items[0]).toHaveAttribute(
-    "style",
-    "background-color: rgb(16, 16, 16); height: 1px; width: 1px;"
-  );
 });
